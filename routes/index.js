7f1c159e3a5edc1cd34fa7d12eefc79d4a1d2997
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-06-16 15:54:38 -0300

    MY07 - Organizando o HEADER com includes

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,9 @@ router.get('/', function(req, res, next) {
     if(err) console.log(err);
     
     res.render('index', 
-      {title: 'Restaurante Saboroso!',
-      menus: results
+      {
+        title: 'Restaurante Saboroso!',
+        menus: results
       }
     );
   });
@@ -17,19 +18,43 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/contacts', function(req, res, next) {
-  res.render('contacts', {title: 'Contato - Restaurante Saboroso!'});
+  res.render('contacts', 
+    {
+      title: 'Contato - Restaurante Saboroso!', 
+      background: 'images/img_bg_3.jpg',
+      headerTitle: 'Diga um oi!'
+  }
+  );
 });
 
 router.get('/menus', function(req, res, next) {
-  res.render('menus', {title: 'Menus - Restaurante Saboroso!'});
+  res.render('menus', 
+    {
+      title: 'Menus - Restaurante Saboroso!', 
+      background: 'images/img_bg_1.jpg',
+      headerTitle: 'Saboreie nosso menu!'
+    }
+  );
 });
 
 router.get('/reservations', function(req, res, next) {
-  res.render('reservations', {title: 'Reservas - Restaurante Saboroso!'});
+  res.render('reservations', 
+    {
+      title: 'Reservas - Restaurante Saboroso!', 
+      background: 'images/img_bg_2.jpg',
+      headerTitle: 'Reserve uma Mesa!'
+    }
+  );
 });
 
 router.get('/services', function(req, res, next) {
-  res.render('services', {title: 'Serviços - Restaurante Saboroso!'});
+  res.render('services', 
+    {
+      title: 'Serviços - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      headerTitle: 'É um prazer poder servir!'
+    }
+  );
 });
 
 module.exports = router;
