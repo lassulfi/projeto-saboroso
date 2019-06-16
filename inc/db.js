2d2989505fd49fdc03ef0671c7f9cac996b49149
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-06-16 14:46:03 -0300

    Conectando o NodeJS com o MySQL

---
+++ inc/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'saboroso',
+    password: 'root',
+    port: 3390
+});
+
+module.exports = connection;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('./../inc/db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  connection.query("SELECT * FROM tb_users ORDER BY name", (err, result) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 module.exports = router;
