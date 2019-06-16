900081592403c8b58d45c59a531f3931c5122181
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-06-16 15:09:52 -0300

    MY05 - Exibindo dados do banco de dados com EJS Embedded Javascript Templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
+var connection = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  connection.query('SELECT * FROM tb_menus ORDER BY title', (err, results) => {
+    if(err) console.log(err);
+    
+    res.render('index', 
+      {title: 'Restaurante Saboroso!',
+      menus: results
+      }
+    );
+  });
+
+  
 });
 
 module.exports = router;
