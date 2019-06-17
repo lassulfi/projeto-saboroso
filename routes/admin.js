8c8facfb9fbca351f0528a9ce5183b6fb32a94f5
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-06-17 13:14:57 -0300

    MY15 - Efetuando Login na Administracao

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var users = require('./../inc/users');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -6,9 +7,22 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-    if(!req.session.views) req.session.views = 0;
-    console.log("SESSION::: ", req.session.views++);
-    res.render("admin/login");
+    users.render(req, res, null);
+});
+
+router.post('/login', function(req, res, next) {
+    if(!req.body.email){
+        users.render(req, res, "Campo e-mail obrigatório");
+    } else if (!req.body.password) {
+        users.render(req, res, "Campo senha obrigatório");
+    } else {
+        users.login(req.body.email, req.body.password).then(user => {
+            req.session.user = user;
+            res.redirect('/admin');
+        }).catch(err => {
+            users.render(req, res, err.message || err);
+        });
+    }
 });
 
 router.get('/contacts', function(req, res, next) {
