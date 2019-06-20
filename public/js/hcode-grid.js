39ecbe1ae2126f619d303f2cdfb7b6fce720b88c
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-06-20 16:02:16 -0300

    MY36 - Trabalhando com eventos do componente

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -1,10 +1,16 @@
 class HCodeGrid {
     constructor(configs) {
+        configs.listeners = Object.assign({
+            afterUpdateClick: (e) => {
+                $('#modal-update').modal('show');
+            }
+        }, configs.listeners)
+
         this._options = Object.assign({}, {
             formCreate: "#modal-create form",
             formUpdate: "#modal-update form",
             btnUpdate: ".btn-update",
-            btnDelete: ".btn-delete"
+            btnDelete: ".btn-delete",
         }, configs);
 
         this.initForms();
@@ -27,26 +33,31 @@ class HCodeGrid {
         });
     }
 
-    initButtons() {
+    fireEvents(name, args) {
+        if(typeof this._options.listeners[name] === 'function') {
+            this._options.listeners[name].apply(this, args);
+        }
+    }
 
+    initButtons() {
         [...document.querySelectorAll(this._options.btnUpdate)].forEach(btn => {
             btn.addEventListener('click', e => {
-            let tr = e.path.find(el => {
-                return (el.tagName.toUpperCase() === 'TR');
-            });
-            let data = JSON.parse(tr.dataset.row);
-            for(let name in data) {
-                let input = this.formUpdate.querySelector(`[name=${name}]`);
-                switch (name) {
-                case "date":
-                    if (input) input.value = moment(data[name]).format('YYYY-MM-DD')
-                    break;
-                default:
-                    if(input) input.value = data[name];
-                    break;
+                let tr = e.path.find(el => {
+                    return (el.tagName.toUpperCase() === 'TR');
+                });
+                let data = JSON.parse(tr.dataset.row);
+                for(let name in data) {
+                    let input = this.formUpdate.querySelector(`[name=${name}]`);
+                    switch (name) {
+                    case "date":
+                        if (input) input.value = moment(data[name]).format('YYYY-MM-DD')
+                        break;
+                    default:
+                        if(input) input.value = data[name];
+                        break;
+                    }
                 }
-            }
-            $('#modal-update').modal('show');
+               this.fireEvents('afterUpdateClick', [e]);
             });
         });
 
