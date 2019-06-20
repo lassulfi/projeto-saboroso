9ed7eeffec3f5e5334a687051bb9039f33bed158
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-06-20 16:18:23 -0300

    MY37 - Refatorando e criando metodo para botao DELETE

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -3,6 +3,21 @@ class HCodeGrid {
         configs.listeners = Object.assign({
             afterUpdateClick: (e) => {
                 $('#modal-update').modal('show');
+            },
+            afterDeleteClick: (e) => {
+                window.location.reload();
+            },
+            afterFormCreate: (e) => {
+                window.location.reload();
+            },
+            afterFormUpdate: (e) => {
+                window.location.reload();
+            }, 
+            onFormCreateCatchError: (e) => {
+                alert("Não foi possível enviar o formulário");
+            },
+            onFormUpdateCatchError: (e) => {
+                alert("Não foi possível enviar o formulário");
             }
         }, configs.listeners)
 
@@ -20,16 +35,16 @@ class HCodeGrid {
     initForms() {
         this.formCreate = document.querySelector(this._options.formCreate);
         this.formCreate.save().then(json => {
-            window.location.reload();
+            this.fireEvents('afterFormCreate');
         }).catch(err => {
-            console.error(err);
+            this.fireEvents('onFormCreateCatchError');
         });
         
         this.formUpdate = document.querySelector(this._options.formUpdate);
         this.formUpdate.save().then(json => {
-            window.location.reload();
+            this.fireEvents('afterFormUpdate');
         }).catch(err => {
-            console.error(err);
+            this.fireEvents('onFormUpdateCatchError');
         });
     }
 
@@ -39,13 +54,19 @@ class HCodeGrid {
         }
     }
 
+    getTrData(e) {
+        let tr = e.path.find(el => {
+            return (el.tagName.toUpperCase() === 'TR');
+        });
+        return JSON.parse(tr.dataset.row);
+    }
+
     initButtons() {
         [...document.querySelectorAll(this._options.btnUpdate)].forEach(btn => {
             btn.addEventListener('click', e => {
-                let tr = e.path.find(el => {
-                    return (el.tagName.toUpperCase() === 'TR');
-                });
-                let data = JSON.parse(tr.dataset.row);
+                this.fireEvents('beforeUpdateClick', [e]);
+
+                let data = this.getTrData(e);
                 for(let name in data) {
                     let input = this.formUpdate.querySelector(`[name=${name}]`);
                     switch (name) {
@@ -63,15 +84,16 @@ class HCodeGrid {
 
         [...document.querySelectorAll(this._options.btnDelete)].forEach(btn => {
             btn.addEventListener('click', e => {
-            let tr = e.path.find(el => {
-                return (el.tagName.toUpperCase() === 'TR');
-            });
-            let data = JSON.parse(tr.dataset.row);
+            this.fireEvents('beforeDeleteClick', [e]);
+
+            let data = this.getTrData(e);
             if(confirm(eval('`' + this._options.deleteMessage + '`'))){
                 fetch(eval('`' + this._options.deleteUrl + '`'), {
                 method: 'delete'
                 }).then(response => response.json())
-                .then(json => window.location.reload());
+                .then(json => {
+                    this.fireEvents('afterDeleteClick')
+                });
             }
             });
         });
