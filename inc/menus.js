4b1a25039a065a8bc7cb701c03c639229bfb9212
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-06-19 21:38:41 -0300

    MY29 - Administracao do Menu = Excluindo dados

--- inc/menus.js
+++ inc/menus.js
@@ -39,5 +39,16 @@ module.exports = {
                 }
             });
         });
+    },
+    delete(id) {
+        return new Promise((resolve, reject) => {
+            connection.query("DELETE FROM tb_menus WHERE id = ?", [id], (err, results) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
     }
 }
